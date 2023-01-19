5f548d3197020246bb2b3232a365bcbf84d1a3f7
Suruka <62789567+suruka@users.noreply.github.com>
2023-01-19 16:37:00 +0800

    Add files via upload

--- prism-nlan.js
+++ prism-nlan.js
@@ -1,12 +1,17 @@
 (function (Prism) {
     var comment = /\'\'.*|#(?!\[).*|\'[\s\S]+?\'/;
     var functions = {
-        pattern: /(?<=@\{[\s\S]*)[^,\s]+(?=[\s\S]*\}\()|(?<=@)([^\s\{\}\:]+?)(?=\()|(?<=@)[\S]+(?=::)/i,
+        pattern: /(?<=@[\s]*\{[\s\S]*)[^,\s]+(?=[\s\S]*\}[\s]*\()|(?<=@[\s]*)([^\s\{\}\:]+?)(?=[\s]*\()|(?<=@[\s]*)[\S]+(?=[\s]*::)/i,
         lookbehind: false,
         greedy: true
     };
+    var classname = {
+        pattern: /(?<=(type|interface)[\s]+#?[\s]*)[^\s#:]*?(?=[\s]*[\{|::])|(?<=::[\s]*\{.*,[\s]*)[\S]+?(?=\W)|(?<=::[\s]*\{[\s]*)[\S]+?(?=\W)|(?<=::)[^\s\{]+?(?=\W)/i,
+        lookbehind: false
+    }
     Prism.languages.nlan = {
         'comment': comment,
-        'function': functions
+        'function': functions,
+        'class-name': classname
     };
 }(Prism));

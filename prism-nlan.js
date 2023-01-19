b24033ff23c46a324411ece25a32373dcd6a8f73
Suruka <62789567+suruka@users.noreply.github.com>
2023-01-19 11:05:25 +0800

    Add files via upload

--- prism-nlan.js
+++ prism-nlan.js
@@ -3,4 +3,17 @@
     Prism.languages.nlan = {
         'comment':comment
     };
+    Prism.languages.insertBefore('nlan', 'keyword', {
+        'generic-function': {
+            pattern: /\b(?!operator\b)[a-z_@]\w*\s*<(?:[^<>]|<[^<>]*>)*>(?=\s*\()/i,
+            inside: {
+                'function': /^\w+/,
+                'generic': {
+                    pattern: /<[\s\S]+/,
+                    alias: 'class-name',
+                    inside: Prism.languages.nlan
+                }
+            }
+        }
+    });
 }(Prism));
